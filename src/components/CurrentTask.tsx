b95389e19eb71f34e0f2085084a40253688d0267
Brenton Beltrami <contact@brentonbeltrami.com>
2021-11-08 22:47:30 -0600

    added current task component

--- src/components/CurrentTask.tsx
+++ src/components/CurrentTask.tsx
@@ -2,144 +2,144 @@ import { useState, useEffect } from 'react';
 import { Task } from '.prisma/client';
 
 interface OwnProps {
-	task: Task;
+  task: Task;
 }
 
 export default function CurrentTask({ task }: OwnProps) {
-	const [isShowingMenu, setIsShowingMenu] = useState(false);
-	const [isShowingAbandon, setIsShowingAbandon] = useState(false);
-	const [confirmDelete, setConfirmDelete] = useState('');
-	const confirmDeleteCheck = `${task.clientName}/${task.name}`;
+  const [isShowingMenu, setIsShowingMenu] = useState(false);
+  const [isShowingAbandon, setIsShowingAbandon] = useState(false);
+  const [confirmDelete, setConfirmDelete] = useState('');
+  const confirmDeleteCheck = `${task.clientName}/${task.name}`;
 
-	useEffect(() => {
-		//console.log('Current Task:', task);
-	}, []);
+  useEffect(() => {
+    //console.log('Current Task:', task);
+  }, []);
 
-	function toggleMenu() {
-		setIsShowingMenu(!isShowingMenu);
-	}
-	function toggleAbandon() {
-		setIsShowingAbandon(!isShowingAbandon);
-	}
-	function resetAllPops() {
-		setIsShowingAbandon(false);
-		setIsShowingMenu(false);
-		setConfirmDelete('');
-	}
+  function toggleMenu() {
+    setIsShowingMenu(!isShowingMenu);
+  }
+  function toggleAbandon() {
+    setIsShowingAbandon(!isShowingAbandon);
+  }
+  function resetAllPops() {
+    setIsShowingAbandon(false);
+    setIsShowingMenu(false);
+    setConfirmDelete('');
+  }
 
-	//TODO: create api for handleSubmit
-	function handleSubmit() {
-		console.log('Task Has Been Submitted');
-		console.log('TODO: This functionality has not been completed');
-	}
-	function abandonTask() {
-		resetAllPops();
-		console.log('Oh The Horror!!! The Task Has Been ABANDONED!!!!');
-		console.log('TODO: This functionality has not been completed');
-	}
+  //TODO: create api for handleSubmit
+  function handleSubmit() {
+    console.log('Task Has Been Submitted');
+    console.log('TODO: This functionality has not been completed');
+  }
+  function abandonTask() {
+    resetAllPops();
+    console.log('Oh The Horror!!! The Task Has Been ABANDONED!!!!');
+    console.log('TODO: This functionality has not been completed');
+  }
 
-	return (
-		<>
-			{isShowingAbandon ? (
-				<div className="absolute top-0 left-0 z-50 bg-black bg-opacity-90 grid place-content-center w-screen h-screen">
-					<div className="bg-gray-200 p-6 rounded-lg max-w-lg text-center">
-						<p className="my-3 text-red-700 font-semibold text-2xl">
-							Are you sure?
-						</p>
-						<p className="text-center mb-8 mx-6">
-							Abandoning this current task will mean you won't be paid for the
-							work done until this point.
-						</p>
-						<div className="flex flex-col my-2">
-							<label className="my-3" htmlFor="delete">
-								Please type
-								<span className="mx-2 py-1 px-2 rounded-lg bg-gray-300 font-medium">
-									{task.clientName}/{task.name}
-								</span>
-								to confirm.
-							</label>
-							<input
-								id="delete"
-								className="mx-6 rounded leading-8 pl-2"
-								type="text"
-								onChange={(e) => {
-									setConfirmDelete(e.target.value);
-								}}
-							/>
-						</div>
-						<div className="mt-4 flex justify-center">
-							<button
-								className="disabled:opacity-20  px-5 py-2 rounded bg-red-800 disabled:hover:bg-red-800 disabled:cursor-default hover:bg-red-600 text-white font-medium transition ease-in-out duration-500"
-								disabled={confirmDelete !== confirmDeleteCheck}
-								onClick={abandonTask}
-							>
-								Yes, Abandon Task
-							</button>
-							<button
-								onClick={resetAllPops}
-								className="px-3 py-1 ml-4 rounded bg-green-600 text-white font-semibold"
-							>
-								Keep working on task.
-							</button>
-						</div>
-					</div>
-				</div>
-			) : (
-				<></>
-			)}
-			<div className="relative">
-				{isShowingMenu ? (
-					<div className="absolute px-3 py-3 z-10 right-2 top-12 -left-20 bg-gray-200 shadow-md rounded">
-						<div className="text-left">
-							<p className="text-lg">{task.clientName}</p>
-							<p className="text-sm">{task.longDesc}</p>
-							<div className="mt-6 flex justify-center">
-								<button
-									onClick={toggleAbandon}
-									className="px-3 py-2 rounded bg-red-800 hover:bg-red-600 text-red-100 font-medium hover:text-white transition ease-in-out duration-500"
-								>
-									Abandon Task
-								</button>
-								<button className="px-3 py-1 ml-4 rounded bg-indigo-300">
-									Request Help
-								</button>
-							</div>
-						</div>
-					</div>
-				) : (
-					<></>
-				)}
-				<div className="bg-blue-100 text-blue-900 shadow p-4 rounded-md max-w-[15rem]">
-					<div className="text-left">
-						<div className="flex justify-between items-start">
-							<h3 className="text-lg">{task.name}</h3>
-							<button onClick={toggleMenu}>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="h-6 w-6 text-gray-400 ml-6"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-									/>
-								</svg>
-							</button>
-						</div>
-						<p className="mb-4 text-green-800">${task.price}</p>
-						<button
-							className="bg-blue-900 hover:bg-blue-700 font-medium transform ease-in-out duration-500 w-full text-white px-3 py-2 rounded"
-							onClick={handleSubmit}
-						>
-							Submit For Review
-						</button>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      {isShowingAbandon ? (
+        <div className="absolute top-0 left-0 z-50 bg-black bg-opacity-90 grid place-content-center w-screen h-screen">
+          <div className="bg-gray-200 p-6 rounded-lg max-w-lg text-center">
+            <p className="my-3 text-red-700 font-semibold text-2xl">
+              Are you sure?
+            </p>
+            <p className="text-center mb-8 mx-6">
+              Abandoning this current task will mean you won't be paid for the
+              work done until this point.
+            </p>
+            <div className="flex flex-col my-2">
+              <label className="my-3" htmlFor="delete">
+                Please type
+                <span className="mx-2 py-1 px-2 rounded-lg bg-gray-300 font-medium">
+                  {task.clientName}/{task.name}
+                </span>
+                to confirm.
+              </label>
+              <input
+                id="delete"
+                className="mx-6 rounded leading-8 pl-2"
+                type="text"
+                onChange={(e) => {
+                  setConfirmDelete(e.target.value);
+                }}
+              />
+            </div>
+            <div className="mt-4 flex justify-center">
+              <button
+                className="disabled:opacity-20  px-5 py-2 rounded bg-red-800 disabled:hover:bg-red-800 disabled:cursor-default hover:bg-red-600 text-white font-medium transition ease-in-out duration-500"
+                disabled={confirmDelete !== confirmDeleteCheck}
+                onClick={abandonTask}
+              >
+                Yes, Abandon Task
+              </button>
+              <button
+                onClick={resetAllPops}
+                className="px-3 py-1 ml-4 rounded bg-green-600 text-white font-semibold"
+              >
+                Keep working on task.
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+      <div className="relative">
+        {isShowingMenu ? (
+          <div className="absolute px-3 py-3 z-10 right-2 top-12 -left-20 bg-gray-200 shadow-md rounded">
+            <div className="text-left">
+              <p className="text-lg">{task.clientName}</p>
+              <p className="text-sm">{task.longDesc}</p>
+              <div className="mt-6 flex justify-center">
+                <button
+                  onClick={toggleAbandon}
+                  className="px-3 py-2 rounded bg-red-800 hover:bg-red-600 text-red-100 font-medium hover:text-white transition ease-in-out duration-500"
+                >
+                  Abandon Task
+                </button>
+                <button className="px-3 py-1 ml-4 rounded bg-indigo-300">
+                  Request Help
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className="bg-blue-100 text-blue-900 shadow p-4 rounded-md max-w-[15rem]">
+          <div className="text-left">
+            <div className="flex justify-between items-start">
+              <h3 className="text-lg">{task.name}</h3>
+              <button onClick={toggleMenu}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-gray-400 ml-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                  />
+                </svg>
+              </button>
+            </div>
+            <p className="mb-4 text-green-800">${task.price}</p>
+            <button
+              className="bg-blue-900 hover:bg-blue-700 font-medium transform ease-in-out duration-500 w-full text-white px-3 py-2 rounded"
+              onClick={handleSubmit}
+            >
+              Submit For Review
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/UserTasksColumn.tsx
+++ src/components/UserTasksColumn.tsx
@@ -11,51 +11,51 @@ import Axios from 'axios';
 import { Task } from '.prisma/client';
 
 interface OwnProps {
-	user: UserProfile;
+  user: UserProfile;
 }
 
 export default function UserTaskColumn({ user }: OwnProps) {
-	const { data, loading, error } = useSingleTask();
-	const { reviewTasks, reviewLoading, reviewError } = useReviewTasks();
-	const { previousTasks, previousLoading, previousError } = usePreviousTasks();
+  const { data, loading, error } = useSingleTask();
+  const { reviewTasks, reviewLoading, reviewError } = useReviewTasks();
+  const { previousTasks, previousLoading, previousError } = usePreviousTasks();
 
-	useEffect(() => {
-		console.log('DATA', data);
-		console.log('Previous', previousTasks);
-	}, [previousTasks]);
+  useEffect(() => {
+    console.log('DATA', data);
+    console.log('Previous', previousTasks);
+  }, [previousTasks]);
 
-	if (loading) return <div>Loading...</div>;
-	return (
-		<div className="flex mt-2">
-			<div>
-				<img
-					className="mx-auto rounded-full h-20 w-20"
-					src={user.picture || ''}
-					alt="User Picture"
-				/>
-				<div className="text-left">
-					<p className="my-3 font-semibold text-gray-400">Current Task</p>
-					{/* @ts-ignore */}
-					{data !== null && data?.message !== 'no task' ? (
-						<div>
-							<CurrentTask task={data as Task} />
-						</div>
-					) : (
-						<div>
-							<TaskEmptyState />
-						</div>
-					)}
+  if (loading) return <div>Loading...</div>;
+  return (
+    <div className="flex mt-2">
+      <div>
+        <img
+          className="mx-auto rounded-full h-20 w-20"
+          src={user.picture || ''}
+          alt="User Picture"
+        />
+        <div className="text-left">
+          <p className="my-3 font-semibold text-gray-400">Current Task</p>
+          {/* @ts-ignore */}
+          {data !== null && data?.message !== 'no task' ? (
+            <div>
+              <CurrentTask task={data as Task} />
+            </div>
+          ) : (
+            <div>
+              <TaskEmptyState />
+            </div>
+          )}
 
-					<p className="mt-6 mb-3 font-semibold text-gray-400">
-						Tasks In Review
-					</p>
-					<TasksInReview tasks={reviewTasks as Task[]} />
-					<p className="mt-6 mb-3 font-semibold text-gray-400">
-						Previous Tasks
-					</p>
-					<PreviousTasks tasks={previousTasks as Task[]} />
-				</div>
-			</div>
-		</div>
-	);
+          <p className="mt-6 mb-3 font-semibold text-gray-400">
+            Tasks In Review
+          </p>
+          <TasksInReview tasks={reviewTasks as Task[]} />
+          <p className="mt-6 mb-3 font-semibold text-gray-400">
+            Previous Tasks
+          </p>
+          <PreviousTasks tasks={previousTasks as Task[]} />
+        </div>
+      </div>
+    </div>
+  );
 }
