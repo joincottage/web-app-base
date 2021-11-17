a0cfd7355a2d8a4d0930445cd83bf08a4d3d7fbb
Brenton Beltrami <contact@brentonbeltrami.com>
2021-11-17 12:33:50 -0600

    Disabled ill do it button on task based on user current task, added empty state

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Theme } from '@material-ui/core/styles';
 import { makeStyles, createStyles } from '@material-ui/styles';
 import Card from '@material-ui/core/Card';
@@ -18,164 +18,169 @@ import CloseIcon from '@material-ui/icons/Close';
 import IllDoIt from './IllDoIt';
 import { UserProfile, useUser } from '@auth0/nextjs-auth0';
 import Axios from 'axios';
+import { useSingleTask } from './../hooks/useSingleTask';
+import TaskCardSkeleton from './TaskCardSkeleton';
 
 interface OwnProps {
-  task: Task;
-  mode: 'freelancer' | 'client';
+	task: Task;
+	mode: 'freelancer' | 'client';
 }
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-      borderRadius: 0,
-    },
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-    ctaButton: {},
-    primaryActionsContainer: {
-      marginLeft: 'auto',
-    },
-    expand: {
-      transform: 'rotate(0deg)',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)',
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
-    paper: {
-      position: 'absolute',
-      width: '400px',
-      minHeight: '405px',
-      backgroundColor: theme.palette.background.paper,
-      borderRadius: '4px',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-    closeIcon: {
-      cursor: 'pointer',
-      position: 'absolute',
-      right: '15px',
-    },
-  })
+	createStyles({
+		root: {
+			width: '100%',
+			borderRadius: 0,
+		},
+		media: {
+			height: 0,
+			paddingTop: '56.25%', // 16:9
+		},
+		ctaButton: {},
+		primaryActionsContainer: {
+			marginLeft: 'auto',
+		},
+		expand: {
+			transform: 'rotate(0deg)',
+			transition: theme.transitions.create('transform', {
+				duration: theme.transitions.duration.shortest,
+			}),
+		},
+		expandOpen: {
+			transform: 'rotate(180deg)',
+		},
+		avatar: {
+			backgroundColor: red[500],
+		},
+		paper: {
+			position: 'absolute',
+			width: '400px',
+			minHeight: '405px',
+			backgroundColor: theme.palette.background.paper,
+			borderRadius: '4px',
+			boxShadow: theme.shadows[5],
+			padding: theme.spacing(2, 4, 3),
+		},
+		closeIcon: {
+			cursor: 'pointer',
+			position: 'absolute',
+			right: '15px',
+		},
+	})
 );
 
 function getModalStyle() {
-  return {
-    top: `50%`,
-    left: `50%`,
-    transform: `translate(-50%, -50%)`,
-  };
+	return {
+		top: `50%`,
+		left: `50%`,
+		transform: `translate(-50%, -50%)`,
+	};
 }
 
 export default function TaskCard({ task, mode }: OwnProps) {
-  const { user } = useUser();
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-  const [open, setOpen] = useState(false);
-  const [modalStyle] = React.useState(getModalStyle);
+	const { user } = useUser();
+	const classes = useStyles();
+	const [expanded, setExpanded] = React.useState(false);
+	const [open, setOpen] = useState(false);
+	const [modalStyle] = React.useState(getModalStyle);
+	const { data, loading, error } = useSingleTask();
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+	const handleExpandClick = () => {
+		setExpanded(!expanded);
+	};
 
-  const handleClickIllDoIt = async () => {
-    setOpen(true);
-    // sa_event('click_IllDoIt');
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const modalBody = (
-    <div style={modalStyle} className={classes.paper}>
-      <CloseIcon
-        onClick={handleClose}
-        className={classes.closeIcon}
-        style={{ color: 'white' }}
-      />
-      <IllDoIt user={user as UserProfile} task={task} />
-    </div>
-  );
+	const handleClickIllDoIt = async () => {
+		setOpen(true);
+		// sa_event('click_IllDoIt');
+	};
+	const handleClose = () => {
+		setOpen(false);
+	};
+	const modalBody = (
+		<div style={modalStyle} className={classes.paper}>
+			<CloseIcon
+				onClick={handleClose}
+				className={classes.closeIcon}
+				style={{ color: 'white' }}
+			/>
+			<IllDoIt user={user as UserProfile} task={task} />
+		</div>
+	);
 
-  return (
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar
-            sx={{ width: 24, height: 24 }}
-            alt="Company logo"
-            src={task.clientImgUrl || ''}
-            aria-haspopup="true"
-          />
-        }
-        action={
-          <Typography variant="h6" style={{ color: 'green' }}>
-            ${task.price}
-          </Typography>
-        }
-        title={task.name}
-        subheader="October 14, 2021"
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {task.shortDesc}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        {task.skills?.split(',').map((skill) => (
-          <Chip key={skill} label={skill} style={{ marginLeft: '5px' }} />
-        ))}
-        {mode === 'freelancer' && (
-          <div className={classes.primaryActionsContainer}>
-            <Button
-              className={classes.ctaButton}
-              variant="outlined"
-              color="primary"
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-            >
-              Learn more
-            </Button>
-          </div>
-        )}
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>{task.longDesc}</Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <div className={classes.primaryActionsContainer}>
-            <Button
-              onClick={handleClickIllDoIt}
-              className={classes.ctaButton}
-              variant="contained"
-              color="primary"
-            >
-              I&apos;ll do it!
-            </Button>
-          </div>
-        </CardActions>
-      </Collapse>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>{modalBody}</Fade>
-      </Modal>
-    </Card>
-  );
+	if (loading) return <TaskCardSkeleton />;
+	return (
+		<Card className={classes.root}>
+			<CardHeader
+				avatar={
+					<Avatar
+						sx={{ width: 24, height: 24 }}
+						alt="Company logo"
+						src={task.clientImgUrl || ''}
+						aria-haspopup="true"
+					/>
+				}
+				action={
+					<Typography variant="h6" style={{ color: 'green' }}>
+						${task.price}
+					</Typography>
+				}
+				title={task.name}
+				subheader="October 14, 2021"
+			/>
+			<CardContent>
+				<Typography variant="body2" color="textSecondary" component="p">
+					{task.shortDesc}
+				</Typography>
+			</CardContent>
+			<CardActions disableSpacing>
+				{task.skills?.split(',').map((skill) => (
+					<Chip key={skill} label={skill} style={{ marginLeft: '5px' }} />
+				))}
+				{mode === 'freelancer' && (
+					<div className={classes.primaryActionsContainer}>
+						<Button
+							className={classes.ctaButton}
+							variant="outlined"
+							color="primary"
+							onClick={handleExpandClick}
+							aria-expanded={expanded}
+						>
+							Learn more
+						</Button>
+					</div>
+				)}
+			</CardActions>
+			<Collapse in={expanded} timeout="auto" unmountOnExit>
+				<CardContent>
+					<Typography paragraph>{task.longDesc}</Typography>
+				</CardContent>
+				<CardActions disableSpacing>
+					<div className={classes.primaryActionsContainer}>
+						<Button
+							disabled={data === null || data.length > 0}
+							onClick={handleClickIllDoIt}
+							className={classes.ctaButton}
+							variant="contained"
+							color="primary"
+						>
+							I&apos;ll do it!
+						</Button>
+					</div>
+				</CardActions>
+			</Collapse>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description"
+				closeAfterTransition
+				BackdropComponent={Backdrop}
+				BackdropProps={{
+					timeout: 500,
+				}}
+			>
+				<Fade in={open}>{modalBody}</Fade>
+			</Modal>
+		</Card>
+	);
 }
--- src/pages/api/discord/notify-task-picked-up.ts
+++ src/pages/api/discord/notify-task-picked-up.ts
@@ -1,65 +1,68 @@
 import { Task } from '.prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import {
-  addUserToChannel,
-  postMessageToChannel,
+	addUserToChannel,
+	postMessageToChannel,
 } from 'src/apiService/discord/channel';
 import { prisma } from 'src/database/prisma';
+import Axios from 'axios';
 
 interface NotifyTaskInterestRequest extends NextApiRequest {
-  body: {
-    name: string;
-    discordChannelId: string;
-    discordUserId: string;
-    task: Task;
-    userEmail: string;
-  };
+	body: {
+		name: string;
+		discordChannelId: string;
+		discordUserId: string;
+		task: Task;
+		userEmail: string;
+	};
 }
 const formatInfo = (name: string) =>
-  `A freelancer has picked up your task! Please welcome ${name} to the channel. ${name}, please request any additional details you need to commence work on this task.`;
+	`A freelancer has picked up your task! Please welcome ${name} to the channel. ${name}, please request any additional details you need to commence work on this task.`;
 
 export default async function (
-  req: NotifyTaskInterestRequest,
-  res: NextApiResponse
+	req: NotifyTaskInterestRequest,
+	res: NextApiResponse
 ) {
-  const { body, method } = req;
-  const name = body.name;
-  const discordChannelId = body.discordChannelId;
-  const discordUserId = body.discordUserId;
-  const task = body.task;
-  const userEmail = body.userEmail;
+	const { body, method } = req;
+	const name = body.name;
+	const discordChannelId = body.discordChannelId;
+	const discordUserId = body.discordUserId;
+	const task = body.task;
+	const userEmail = body.userEmail;
 
-  if (method !== 'POST') {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${method} Not Allowed`);
+	if (method !== 'POST') {
+		res.setHeader('Allow', ['POST']);
+		res.status(405).end(`Method ${method} Not Allowed`);
 
-    return;
-  }
+		return;
+	}
 
-  try {
-    console.log(`Updating task in DB with Prisma`);
-    await prisma.task.update({
-      where: {
-        id: task.id,
-      },
-      data: {
-        status: 'in_progress',
-      },
-    });
-    // await prisma.user.update({
-    //   where: {
-    //     email: userEmail,
-    //   },
-    //   data: {
-    //     currentTaskId: task.id,
-    //   },
-    // });
-    console.log('Task successfully updated in DB');
-    await postMessageToChannel(discordChannelId, formatInfo(name));
-    await addUserToChannel(discordChannelId, discordUserId);
-    res.status(200).json({ message: 'success' });
-  } catch (error) {
-    console.error(`Failed posting info to discord`, error);
-    res.status(500).json({ message: 'Failed to post info' });
-  }
+	try {
+		//TODO: Securely fetch user email
+		console.log(`Updating task in DB with Prisma`);
+		await prisma.task.update({
+			where: {
+				id: task.id,
+			},
+			data: {
+				status: 'in_progress',
+				userId: userEmail,
+			},
+		});
+		// await prisma.user.update({
+		//   where: {
+		//     email: userEmail,
+		//   },
+		//   data: {
+		//     currentTaskId: task.id,
+		//   },
+		// });
+		console.log('Task successfully updated in DB');
+		await postMessageToChannel(discordChannelId, formatInfo(name));
+		await addUserToChannel(discordChannelId, discordUserId);
+		res.status(200).json({ message: 'success' });
+	} catch (error) {
+		console.error(`Failed posting info to discord`, error);
+		res.status(500).json({ message: 'Failed to post info' });
+	}
 }
