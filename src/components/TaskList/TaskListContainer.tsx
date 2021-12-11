34cb4790143a2fedec86e883262796deb9c68053
Brenton Beltrami <contact@brentonbeltrami.com>
2021-12-11 10:59:31 -0600

    Filter bar is now stickied at top of task list

--- src/components/TaskList/TaskListContainer.tsx
+++ src/components/TaskList/TaskListContainer.tsx
@@ -1,14 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, ChangeEvent } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Avatar } from '@material-ui/core';
 import TaskList from 'src/components/TaskList';
 import { Client } from '.prisma/client';
 import { AppDataContext } from '../../contexts/AppContext';
+import TaskTypeFilter from './TaskTypeFilter';
+import { Input, InputAdornment } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import setActiveSearchTerm from 'src/actions/setActiveSearchTerm';
 
 interface OwnProps {}
 
 export default function TaskListContainer({}: OwnProps) {
   const { state, dispatch } = useContext(AppDataContext);
+  const handleSearchTermChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setActiveSearchTerm(e.target.value));
+  };
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <div
@@ -32,6 +39,21 @@ export default function TaskListContainer({}: OwnProps) {
         <Typography variant="h5" style={{ paddingRight: '30px' }}>
           {state.selectedClient.name}
         </Typography>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Input
+          placeholder="Search skills"
+          className=" text-sm font-light text-gray-700 bg-gray-200 py-1 px-2 rounded-full"
+          style={{ height: '32px', width: '200px' }}
+          disableUnderline
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          }
+          onChange={handleSearchTermChange}
+        />
+        <TaskTypeFilter />
       </div>
       <div
         style={{
--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AppDataContext } from '../../contexts/AppContext';
 import TaskCardSkeleton from 'src/components/emptystates/TaskCardEmptyState';
 import TaskCard from './TaskCard';
@@ -6,12 +6,8 @@ import Divider from '@material-ui/core/Divider';
 import { Task } from '@prisma/client';
 import useTasks from 'src/hooks/useTasks';
 import { TASK_QUEUED } from 'src/constants/task-stages';
-import { Fade, InputAdornment } from '@material-ui/core';
+import { Fade } from '@material-ui/core';
 import setTasksInQueue from 'src/actions/setTasksInQueue';
-import TaskTypeFilter from './TaskTypeFilter';
-import { Input } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
-import setActiveSearchTerm from 'src/actions/setActiveSearchTerm';
 
 interface OwnProps {
   //DESTRUCTUREDPROP: [];
@@ -48,27 +44,8 @@ export default function TaskList({}: OwnProps) {
     }
   }, [data]);
 
-  const handleSearchTermChange = (e: ChangeEvent<HTMLInputElement>) => {
-    dispatch(setActiveSearchTerm(e.target.value));
-  };
-
   return (
     <div>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Input
-          placeholder="Search skills"
-          className=" text-sm font-light text-gray-700 bg-gray-200 py-1 px-2 rounded-full"
-          style={{ height: '32px', width: '200px' }}
-          disableUnderline
-          startAdornment={
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          }
-          onChange={handleSearchTermChange}
-        />
-        <TaskTypeFilter />
-      </div>
       {loading ? (
         <div>
           <TaskCardSkeleton />
