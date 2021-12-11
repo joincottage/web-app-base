9852625972160873e9dcd9d780b120d501a27cf4
Hunter Hodnett <hchodnett@gmail.com>
2021-12-11 15:51:00 -0600

    Revert "Upgraded to TailwindCSS V3"
    This reverts commit 6808ed37f2e953c8c53e49fd3811d19f487eab24.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  content: [
+  mode: 'jit',
+  purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
@@ -80,6 +82,9 @@ module.exports = {
         },
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
 };
