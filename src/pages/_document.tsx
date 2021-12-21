705ea41b5f12e93d0eca1ee1c86626e0b712be32
Hunter Hodnett <hchodnett@gmail.com>
2021-12-20 22:09:31 -0500

    Add ability to report a bug via Marker.io

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,18 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap"
             rel="stylesheet"
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.markerConfig = {
+    destination: '61c142124de49317be18fdf6', 
+    source: 'snippet'
+  };
+!function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
+    `,
+            }}
+            type="text/javascript"
+          />
         </Head>
         <body>
           <Main />
